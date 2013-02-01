803a2df5ed63bb1f6a0c3ba3acb156ca7de1f263
Eric Jacobs <adminericjacobs@gmail.com>
2013-02-01 00:54:16 -0800

    Removed _parseAlias, colapsed if blocks into for loop

--- sudo.py
+++ sudo.py
@@ -18,39 +18,29 @@ def ls():
     ret = {}
     f = open('/home/ej321278/salt/_modules/sudoers', 'r')
     #defaultsRE = re.compile("^\s*Defaults")
-    hostAliasRE = re.compile("^\s*Host_Alias")
-    userAliasRE = re.compile("^\s*User_Alias")
-    cmndAliasRE = re.compile("^\s*Cmnd_Alias")
+    #hostAliasRE = re.compile("^\s*Host_Alias")
+    #userAliasRE = re.compile("^\s*User_Alias")
+    #cmndAliasRE = re.compile("^\s*Cmnd_Alias")
     aliasRE = re.compile("(\w+)(?=\s*?\=)")
     cmndAlias = {}
     userAlias = {}
     hostAlias = {}
-    #aliaslist = [defaultsRE, hostAliasRE, userAliasRE, cmndAliasRE]
     for line in f.read().splitlines():
-        if(hostAliasRE.search(line)):
-            members = line.split('=')[1].split(",")
-            members = [m.lstrip().rstrip() for m in members]
-            alias = aliasRE.search(line).group(1)
-            if alias in hostAlias:
-                hostAlias[alias] += members
-            else:
-                hostAlias[alias] = members
-        if(userAliasRE.search(line)):
-            members = line.split('=')[1].split(",")
-            members = [m.lstrip().rstrip() for m in members]
-            alias = aliasRE.search(line).group(1)
-            if alias in userAlias:
-                userAlias[alias] += members
-            else:
-                userAlias[alias] = members
-        if(cmndAliasRE.search(line)):
-            members = line.split('=')[1].split(",")
-            members = [m.lstrip().rstrip() for m in members]
-            alias = aliasRE.search(line).group(1)
-            if alias in cmndAlias:
-                cmndAlias[alias] += members
-            else:
-                cmndAlias[alias] = members
+        ret = {'Host_Alias': hostAlias,
+               'User_Alias': userAlias,
+               'Cmnd_Alias': cmndAlias}
+        if not line:
+            continue
+        if line.startswith('#'):
+            continue
+        for key, val in ret.iteritems():
+            if line.startswith(key):
+                members = line.split('=')[1].split(",")
+                members = [m.lstrip().rstrip() for m in members]
+                alias_name = aliasRE.search(line).group(1)
+                if alias_name in val:
+                    val[alias_name] += members
+                else:
+                    val[alias_name] = members
     f.close()
-    ret = {'hostAlias': hostAlias, 'userAlias': userAlias, 'cmndAlias': cmndAlias}
     return ret
